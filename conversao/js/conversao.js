7fac5287ef3e84dcc281a43b798f4dd904143f57
Gisele Leite <156721087+g8sele@users.noreply.github.com>
2024-03-25 16:23:00 -0300

    Add files via upload

---
+++ conversao/js/conversao.js
@@ -0,0 +1,13 @@
+//DOM
+
+const real = document.querySelector('#real')
+const calcular = document.querySelector('#calcular')
+const resultado = document.querySelector('#resultado')
+
+//EVENTOS
+
+calcular.addEventListener('click', ()=>{
+    valor = real.value
+    dolar =  valor/4.97
+    resultado.textContent = `O valor em US$ ${dolar.toFixed(2)}`
+})

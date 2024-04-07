7c9621c3941397204ada7d0161513450d5a3a871
Gisele Leite <156721087+g8sele@users.noreply.github.com>
2024-04-07 20:44:04 -0300

    script.js

--- media escolar/js/script.js
+++ media escolar/js/script.js
@@ -1,19 +1,19 @@
 //dom
-const respostanome = document.querySelector('#respostanome')
-const nota1 = document.querySelector('#nota1')
-const nota2 = document.querySelector('#nota2')
-const nota3 = document.querySelector('#nota3')
-const button = document.querySelector('#button')
-const resultado = document.querySelector('#resultado')
+const respostanome = document.querySelector('#respostanome');
+const nota1 = document.querySelector('#nota1');
+const nota2 = document.querySelector('#nota2');
+const nota3 = document.querySelector('#nota3');
+const button = document.querySelector('#button');
+const resultado = document.querySelector('#resultado');
 
 //evento
 button.addEventListener('click', ()=>{
-    n1 = Number(nota1.value)
-    n2 = Number(nota2.value)
-    n3 = Number(nota3.value)
+    let n1 = Number(nota1.value);
+    let n2 = Number(nota2.value);
+    let n3 = Number(nota3.value);
 
-    soma = n1+n2+n3
-    media = soma/3
+    let soma = n1 + n2 + n3;
+    let media = soma / 3;
 
-    resultado.textContent = `${nome.value} a sua média final é ${media.toFixed(1)}`
-})
+    resultado.textContent = `${respostanome.value}, sua média final é ${media.toFixed(1)}`;
+});
